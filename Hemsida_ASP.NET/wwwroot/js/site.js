4f5d5be7f26f450e18fe4121d0d6f2ea61b90a4d
Ellen Olsson <111755284+Nenn7@users.noreply.github.com>
2023-05-16 18:11:56 +0200

    JS validation and view tweaks

--- Hemsida_ASP.NET/wwwroot/js/site.js
+++ Hemsida_ASP.NET/wwwroot/js/site.js
@@ -16,3 +16,42 @@
         }
     })
 } catch { }
+
+
+let elements = document.querySelectorAll('[data-val="true"]');
+
+for (let element of elements) {
+    element.addEventListener("keyup", function (e) {
+        switch (e.target.type) {
+            case 'text':
+                textValidator(e.target, 2);
+                break;
+            case 'password':
+                passwordValidator(e.target);
+                break;
+        }
+    })
+}
+
+function textValidator(target, minLength) {
+
+    //const regEx = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    // || !regEx.test(target.value
+
+    if (target.value.length < minLength)
+            document.querySelector(`[data-valmsg-for="${target.id}"]`).innerHTML = `${target.id} is invalid`;
+        else
+            document.querySelector(`[data-valmsg-for="${target.id}"]`).innerHTML = "";     
+        }
+
+function passwordValidator(target) {
+
+    const regEx = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s])[^\s]{8,}$/;
+
+    if (!regEx.test(target.value))
+        document.querySelector(`[data-valmsg-for="${target.id}"]`).innerHTML = `${target.id} is invalid`;
+    else
+        document.querySelector(`[data-valmsg-for="${target.id}"]`).innerHTML = "";
+    }
+         
+            
