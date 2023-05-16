fce9c4844b578d420bd4de6abd7b716eb4db93ad
Ellen Olsson <111755284+Nenn7@users.noreply.github.com>
2023-05-16 18:12:12 +0200

    General small changes

--- Hemsida_ASP.NET/Program.cs
+++ Hemsida_ASP.NET/Program.cs
@@ -1,30 +1,31 @@
+using Hemsida_ASP.NET.Contexts;
+using Hemsida_ASP.NET.Models.Identities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<IdentityContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("Identity")));
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("Standard")));
 
 
-
-
-
-
-
+builder.Services.AddIdentity<AppUser, IdentityRole>(x =>
+{
+	x.SignIn.RequireConfirmedAccount = false;
+	x.User.RequireUniqueEmail = true;
+	x.Password.RequiredLength = 5;
+}).AddEntityFrameworkStores<IdentityContext>(); 
 
 
 
 var app = builder.Build();
-
-
-
 app.UseHsts();
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapControllerRoute(
 	name: "default",
 	pattern: "{controller=Home}/{action=Index}/{id?}");
